ce2612a5d78daf9044433736e7fa52d7b33e1852
Paulo Figueiredo <by.paullo@gmail.com>
2022-05-02 13:45:19 -0300

    Change conditional logic

--- game.php
+++ game.php
@@ -2,12 +2,12 @@
 
 function printGame($map, $winner = "Niguém") {
     system("clear");
+    echo "Vencedor: $winner. \n";
     echo $map[0][0] . "|" . $map[0][1] . "|" . $map[0][2] . "\n";
     echo $map[1][0] . "|" . $map[1][1] . "|" . $map[1][2] . "\n";
     echo $map[2][0] . "|" . $map[2][1] . "|" . $map[2][2] . "\n";
     echo "\n";
-    echo "Vencedor: $winner. \n";
-}
+}   
 
 $map = [
     ["_", "_", "_"],
@@ -29,48 +29,65 @@ while (true) {
 
     $map[$choices[1]][$choices[2]] = $choices[0];
 
-    //check horizontal X
-    if ($map[0][0] == $choices[0] && $map[0][1] == $choices[0] && $map[0][2] == $choices[0]) {
-        printGame($map, $choices[0]);
-        break;
+    $vencedor = null;
+
+    for ($i=0; $i < 3; $i++) { 
+        $X = 0;
+        $O = 0;
+        for ($j=0; $j < 3; $j++) { 
+            if ($map[$i][$j] == 'x') $X++;
+            if ($map[$i][$j] == 'o') $O++;
+        }
+        if ($X == 3) $vencedor = "X";
+        else if ($O == 3) $vencedor = "O";
     }
 
-    if ($map[1][0] == $choices[0] && $map[1][1] == $choices[0] && $map[1][2] == $choices[0]) {
-        printGame($map, $choices[0]);
-        break;
+    
+    for ($i=0; $i < 3; $i++) {
+        $X = 0;
+        $O = 0;
+        for ($j=0; $j < 3; $j++) { 
+            if ($map[$j][$i] == 'x') $X++;
+            if ($map[$j][$i] == 'o') $O++;
+        }
+        if ($X == 3) $vencedor = "X";
+        else if ($O == 3) $vencedor = "O";
     }
 
-    if ($map[2][0] == $choices[0] && $map[2][1] == $choices[0] && $map[2][2] == $choices[0]) {
-        printGame($map, $choices[0]);
-        break;
+    $X = 0;
+    $O = 0;
+    $j = 0;
+    $i = 0;
+    while ($i <= 2) {
+        if ($map[$i][$j] == 'x') $X++;
+        if ($map[$i][$j] == 'o') $O++;
+        if ($X == 3) $vencedor = "X";
+        else if ($O == 3) $vencedor = "O";
+        $i++;
+        $j++;
     }
 
-    //check vertical X
-    if ($map[0][0] == $choices[0] && $map[1][0] == $choices[0] && $map[2][0] == $choices[0]) {
-        printGame($map, $choices[0]);
-        break;
+
+    $X = 0;
+    $O = 0;
+    $j = 2;
+    $i = 0;
+    while ($i <= 2) {
+        if ($map[$i][$j] == 'x') $X++;
+        if ($map[$i][$j] == 'o') $O++;
+        if ($X == 3) $vencedor = "X";
+        else if ($O == 3) $vencedor = "O";
+        $i++;
+        $j--;
     }
 
-    if ($map[0][1] == $choices[0] && $map[1][1] == $choices[0] && $map[2][1] == $choices[0]) {
-        printGame($map, $choices[0]);
-        break;
-    }
-
-    if ($map[0][2] == $choices[0] && $map[1][2] == $choices[0] && $map[2][2] == $choices[0]) {
-        printGame($map, $choices[0]);
-        break;
-    }
-
-    //check diagonal X
-    if ($map[0][0] == $choices[0] && $map[1][1] == $choices[0] && $map[2][2] == $choices[0]) {
-        printGame($map, $choices[0]);
-        break;
-    }
-
-    if ($map[0][2] == $choices[0] && $map[1][1] == $choices[0] && $map[2][0] == $choices[0]) {
-        printGame($map, $choices[0]);
-        break;
+    if ($vencedor) {
+        printGame($map, $vencedor);
+        sleep(5);
+        $map = [
+            ["_", "_", "_"],
+            ["_", "_", "_"],
+            [" ", " ", " "]
+        ];
     }
 }
-    
-sleep(2);
